caed9bcec5724ad3139bb317e368c8cbed3a6602
Zhouce <czc2922233@gmail.com>
2018-11-05 10:35:32 -0600

    Create bean
    Use java bean to set the resource and let the uri get the bean

---
+++ src/main/java/com/zhouce/rest/webservices/restfulwebservices/HelloWorldBean.java
@@ -0,0 +1,20 @@
+package com.zhouce.rest.webservices.restfulwebservices;
+
+public class HelloWorldBean {
+	private String message;
+	public HelloWorldBean(String message) {
+		this.message = message;
+	}
+	// if this bean doesn't have the getter function, the uri cannot be access to get the resource, it will return http status = 500
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	// here we use the format to make the String result as a json
+	@Override
+	public String toString() {
+		return String.format("HelloWorldBean [message=%s]", message);
+	}
+}
--- src/main/java/com/zhouce/rest/webservices/restfulwebservices/HelloWorldController.java
+++ src/main/java/com/zhouce/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -3,6 +3,7 @@ package com.zhouce.rest.webservices.restfulwebservices;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,12 +14,24 @@ public class HelloWorldController {
 	// GET
 	// URI   -/hello-world
 	// method return "helloworld"
-	@RequestMapping(method = RequestMethod.GET, path = "/hello-world")
+	@RequestMapping (method = RequestMethod.GET, path = "/hello-world")
 	// this is as a simple serivce that provide the resource, the controller get whatever you return
-	public Map<String, String> helloWorld() {
+	public Map<String, String> hello_World() {
 		Map<String, String> map = new HashMap<>();
 		map.put("WTF", "HelloWorld");
 		return map;
 	}
+	// a easier way to use get method
+	@GetMapping (path = "/helloworld")
+	public String helloWorld() {
+		return "Hello World";
+	}
+	
+	//hello-world-bean
+	@GetMapping (path = "/hello-world-bean")
+	public HelloWorldBean helloWorldBean(String message) {
+		return new HelloWorldBean("Hello World : this is created by bean");
+	}
+	
 }
  
