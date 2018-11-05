1740497c55f06f47c448d2f4057549a29ece8afe
Zhouce <czc2922233@gmail.com>
2018-11-05 10:16:02 -0600

    HelloWorld Service
    First try to write Spring service and Controller

--- src/main/java/com/zhouce/rest/webservices/restfulwebservices/HelloWorldController.java
+++ src/main/java/com/zhouce/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -1,5 +1,24 @@
 package com.zhouce.rest.webservices.restfulwebservices;
 
-public class HelloWorldController {
+import java.util.HashMap;
+import java.util.Map;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+// 1.Tell spring that this is a controller
+@RestController
+public class HelloWorldController {
+	// GET
+	// URI   -/hello-world
+	// method return "helloworld"
+	@RequestMapping(method = RequestMethod.GET, path = "/hello-world")
+	// this is as a simple serivce that provide the resource, the controller get whatever you return
+	public Map<String, String> helloWorld() {
+		Map<String, String> map = new HashMap<>();
+		map.put("WTF", "HelloWorld");
+		return map;
+	}
 }
+ 
