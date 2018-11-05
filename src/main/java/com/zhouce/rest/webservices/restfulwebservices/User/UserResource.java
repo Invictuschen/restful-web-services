559baa0c848c480bafa65545c8cbbdbf14caddb1
Zhouce <czc2922233@gmail.com>
2018-11-05 17:08:07 -0600

    Post method
    Increase post method into the backend.

--- src/main/java/com/zhouce/rest/webservices/restfulwebservices/User/UserResource.java
+++ src/main/java/com/zhouce/rest/webservices/restfulwebservices/User/UserResource.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -23,5 +25,12 @@ public class UserResource {
 	@GetMapping(path = "/users/{uid}") 
 	public User retrieveUser(@PathVariable int uid) { // 这个@pathvariable是当用变量在url时必用的annotation
 		return userdata.findOne(uid);
+	}
+	
+	//	input - details of user
+	//  output - CREATED &Return the created URI
+	@PostMapping("/users")
+	public void createUser(@RequestBody User user) {
+		User savedUser = userdata.save(user);
 	}
 }
